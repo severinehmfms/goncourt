a1f63a4c853db094814dbb2546e4b79bb09da061
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-09-15 14:40:55 +0200

    ajout des exceptions pour book_dao

--- daos/book_dao.py
+++ daos/book_dao.py
@@ -47,14 +47,15 @@ class BookDao(Dao[Book]):
            (ou None s'il n'a pu être trouvé)"""
         book: Optional[Book]
 
-        with Dao.connection.cursor() as cursor:
-            sql = "SELECT * FROM livre WHERE id_livre = %s"
-            cursor.execute(sql, (id_book,))
-            record = cursor.fetchone()
-        if record is not None:
-            book = self.book_from_db(record)
-        else:
-            book = None
+        try:
+            with Dao.connection.cursor() as cursor:
+                sql = "SELECT * FROM livre WHERE id_livre = %s"
+                cursor.execute(sql, (id_book,))
+                record = cursor.fetchone()
+            if record is not None:
+                book = self.book_from_db(record)
+        except Exception as e:
+            print(f"Exception : {e}")
 
         return book
 
@@ -62,27 +63,30 @@ class BookDao(Dao[Book]):
         """Renvoie l'ensemble des personnages de la BD."""
         books_list: list[Book] = []
 
-        with Dao.connection.cursor() as cursor:
+        try:
+            with Dao.connection.cursor() as cursor:
 
-            sql = ("""\
-                    SELECT * FROM livre L 
-                    LEFT JOIN auteur A 
-                    ON L.id_auteur = A.id_auteur 
-                    LEFT JOIN editeur E
-                    ON L.id_editeur = E.id_editeur 
-                    """
-                   )
-            if id_selection is None:
-                cursor.execute(sql)
-            else:
-                print("Recherche des livres via la sélection : Fonctionnalité à implémenter!")
-                #sql += "WHERE id_livre = %s"
-                #cursor.execute(sql, (id_book,))
+                sql = ("""\
+                        SELECT * FROM livre L 
+                        LEFT JOIN auteur A 
+                        ON L.id_auteur = A.id_auteur 
+                        LEFT JOIN editeur E
+                        ON L.id_editeur = E.id_editeur 
+                        """
+                       )
+                if id_selection is None:
+                    cursor.execute(sql)
+                else:
+                    print("Recherche des livres via la sélection : Fonctionnalité à implémenter!")
+                    #sql += "WHERE id_livre = %s"
+                    #cursor.execute(sql, (id_book,))
 
-            records = cursor.fetchall()
+                records = cursor.fetchall()
 
-        for record in records:
-            books_list.append(self.book_from_db(record))
+            for record in records:
+                books_list.append(self.book_from_db(record))
+        except Exception as e:
+            print(f"Exception : {e}")
 
         return books_list
 
