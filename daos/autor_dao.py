f110b7ede59d751afc84ff22252910f3bf39aa46
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-09-15 12:01:05 +0200

    Ajout de autor_dao

---
+++ daos/autor_dao.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+"""
+Classe Dao[Student]
+"""
+from daos.dao import Dao
+from dataclasses import dataclass
+from typing import Optional
+from models.autor import Autor
+
+
+@dataclass
+class AutorDao(Dao[Autor]):
+
+    @staticmethod
+    def autor_from_db(record) -> Autor:
+        """Construit un auteur du modèle d'après son entité en BD"""
+        autor: Autor = Autor(record['nom'], record['prenom'], record['biographie'])
+        autor.id = record['id_auteur']
+        return autor
+
+    def read(self, id_autor: int) -> Optional[Autor]:
+        """Renvoit l'auteur correspondant à l'entité dont la clé primaire est id
+           (ou None s'il n'a pu être trouvé)"""
+        autor: Optional[Autor]
+
+        with Dao.connection.cursor() as cursor:
+            sql = "SELECT * FROM auteur A WHERE A.id_auteur = %s"
+            cursor.execute(sql, (id_autor,))
+            record = cursor.fetchone()
+        if record is not None:
+            autor = self.autor_from_db(record)
+        else:
+            autor = None
+
+        return autor
+
+    def read_all(self) -> list[Autor]:
+        """Renvoit l'ensemble des auteurs de la BD correspondant à l'entité dont l'id est id_autor
+           si ce paramètre est fourni."""
+        autors_list: list[Autor] = []
+
+        with Dao.connection.cursor() as cursor:
+           sql = "SELECT * FROM auteur;"
+           cursor.execute(sql)
+
+           records = cursor.fetchall()
+
+        for record in records:
+            autors_list.append(self.autor_from_db(record))
+
+        return autors_list
+
+
+    def create(self, autor: Autor) -> int:
+        print("Méthode non implémentée")
+
+
+    def update(self, autor: Autor) -> bool:
+        print("Méthode non implémentée")
+
+
+    def delete(self, autor: Autor) -> bool:
+        print("Méthode non implémentée")
+
