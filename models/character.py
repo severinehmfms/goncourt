64fbbfaecb382de1df901ef97650b6fd04539015
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-09-15 10:35:04 +0200

    Création du modèle character.py

---
+++ models/character.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Character:
+    """Personnage du livre"""
+    name: str
+    first_name: str
+
+    def __init__(self, name: str, first_name="") -> None:
+        self.name = name
+        self.first_name = first_name
+
+    def __str__(self) -> str:
+        return f"{self.name} {self.last_name}"
+
