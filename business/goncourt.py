bc10afdf8feadbb9a84c4e25255256825119099d
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-09-15 13:09:54 +0200

    Ajout de la partie métier, pour autor

---
+++ business/goncourt.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+"""
+Classe Goncourt
+"""
+
+from dataclasses import dataclass, field
+from datetime import date
+from typing import Optional
+
+from daos.autor_dao import AutorDao
+from daos.editor_dao import EditorDao
+from models.autor import Autor
+from models.editor import Editor
+
+
+@dataclass
+class Goncourt:
+    """Couche métier de l'application du prix Goncourt,
+    reprenant les cas d'utilisation et les spécifications fonctionnelles """
+
+    def get_autor_by_id(self, id_autor: int) -> Optional[Autor]:
+        autor_dao: AutorDao = AutorDao()
+        return autor_dao.read(id_autor)
+
+    def get_autors_list(self) -> list[Autor]:
+        autor_dao: AutorDao = AutorDao()
+        return autor_dao.read_all()
+
