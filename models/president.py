bccba1264b432fba7f18b79875b6200e16831f7d
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-09-16 13:25:08 +0200

    Ajout de l'id au jury : fichier president

--- models/president.py
+++ models/president.py
@@ -7,5 +7,10 @@ from models.jury import Jury
 class President(Jury):
     """Président du jury"""
 
+    def __init__(self, last_name: str, first_name: str) -> None:
+        self.last_name = last_name
+        self.first_name = first_name
+        self.id_jury = 0
+
     def __str__(self) -> str:
         return f"Président : {self.first_name} {self.last_name}"
