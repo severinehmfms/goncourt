ff1fd76bafb5886387eb6a56fef0d99c61561487
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-09-16 10:20:17 +0200

    Ajout dans les insertions sql des livres de la première sélection

--- insertions_table.sql
+++ insertions_table.sql
@@ -45,3 +45,22 @@ INSERT INTO `personnage` (`id_personnage`, `nom`, `prenom`, `id_livre`) VALUES
 INSERT INTO `personnage` (`id_personnage`, `nom`, `prenom`, `id_livre`) VALUES
 (14, 'Olivier', '', 7);
 
+
+''' Insertion des 16 livres dans la première sélection '''
+INSERT INTO `choix` (id_livre, id_jury, num_selection) VALUES
+(1, 1, 1),
+(2, 1, 1),
+(3, 1, 1),
+(4, 1, 1),
+(5, 1, 1),
+(6, 1, 1),
+(7, 1, 1),
+(8, 1, 1),
+(9, 1, 1),
+(10, 1, 1),
+(11, 1, 1),
+(12, 1, 1),
+(13, 1, 1),
+(14, 1, 1),
+(15, 1, 1),
+(16, 1, 1);
