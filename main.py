34d298072ec45f7acc384223bb5bc1417ac88e2a
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-09-17 16:00:13 +0200

    passage en revue en cours du main, pour enlever les avertissements

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ Application de gestion du prix Goncourt 2026
 """
 
 from business.goncourt import Goncourt
-from daos import jury_dao
 from models.book import Book
 from models.jury import Jury
 from models.president import President
@@ -23,7 +22,7 @@ def get_int_input(prompt, min_int, max_int):
     return int(user_input)
 
 
-def input_menu(items, multiline = False):
+def input_menu(items, multiline=False):
     """Demande à l'utilisateur de choisir un item dans un menu.
     Le choix 0 permet toujours de quitter.
     Les options fournies en paramètre commencent à 1.
@@ -41,28 +40,25 @@ def input_menu(items, multiline = False):
     return get_int_input(menu, 0, len(items))
 
 
-def show_jury(goncourt_instance:Goncourt):
+def show_jury(goncourt_instance: Goncourt):
     print("****************** Méthode qui affiche la liste des membres du jury ******************")
     list_jurys: list[Jury] = goncourt_instance.get_jurys_list()
     for j in list_jurys:
         print(j)
 
-def show_selections(goncourt_instance:Goncourt):
+
+def show_selections(goncourt_instance: Goncourt):
     print("****************** Sélections déjà passées ******************")
     num_selection = get_int_input("Entrez le numéro de la sélection demandée : 1,2,3 ou 4 pour voir le lauréat\n", 1, 4)
     selection: Selection = goncourt_instance.get_selection_by_id(num_selection)
     print(selection)
-    if (not goncourt_instance.is_selection_already(num_selection)):
+    if not goncourt_instance.is_selection_already(num_selection):
         print(
             f"Cette sélection n'a pas encore été effectuée, il faudra attendre le {selection.selection_date} pour voir les livres qui auront été choisis")
 
-#def choice_for_selection(goncourt_instance:Goncourt):
+# def choice_for_selection(goncourt_instance:Goncourt):
 
-
-
-#def choice_laureat(goncourt_instance:Goncourt)
-
-
+# def choice_laureat(goncourt_instance:Goncourt)
 
 
 def main() -> None:
@@ -86,7 +82,7 @@ Prix Goncourt 2026
     ]
     choix = -1
     # On ne sort pas du programme tant que l'utilisateur ne l'a pas spécifié
-    while (choix != 0):
+    while choix != 0:
         # On récupère le choix de l'utilisateur par rapport au menu
         choix = input_menu(menu, True)
 
@@ -110,20 +106,20 @@ Prix Goncourt 2026
                 num_selection = 2
 
                 # On vérifie si la deuxième ET la troisième sélection ont pas déjà été faites
-                if (is_selection_2_already and is_selection_3_already):
+                if is_selection_2_already and is_selection_3_already:
                     print("ERREUR - Les deux sélections ont déjà été renseignées, ce n'est plus possible de le faire.")
                     input("Appuyez sur la touche 'Entrée' pour retourner au menu")
                     continue
 
                 # Si la 2ème sélection a déjà été faite, on va faire la troisième
-                elif (is_selection_2_already):
+                elif is_selection_2_already:
                     num_selection = 3
 
                 print(f"Vous allez choisir les livres pour la sélection numéro {num_selection} : ")
                 selection: Selection = goncourt_instance.get_selection_by_id(num_selection)
                 print(selection)
 
-                #Pour la deuxième sélection, on va afficher les livres de la 1ère sélection, et pour la troisième sélection on va afficher les livres de la 2ème sélection !
+                # Pour la deuxième sélection, on va afficher les livres de la 1ère sélection, et pour la troisième sélection on va afficher les livres de la 2ème sélection !
                 print("Voici les livres disponibles : ")
                 list_books_availables: list[Book] = goncourt_instance.get_books_by_selection(num_selection-1)
                 for b in list_books_availables:
@@ -131,7 +127,7 @@ Prix Goncourt 2026
 
                 print(f"Vous allez choisir {selection.nb_books} livres parmi les livres proposés ci-dessus.")
                 list_id_books_selected: list[int] = []
-                for i in range(0, selection.nb_books):
+                for _ in range(0, selection.nb_books):
                     # On demande au président le numéro du livre qu'il souhaite ajouter à la sélection
                     # TODO Mis 16 en dur car c'est le numéro du dernier livre, mais idéalement il faudrait vérifier que le numéro de livre choisi
                     # Fait partie de la liste des livres de la sélection précédente (list_books_availables)
@@ -146,18 +142,18 @@ Prix Goncourt 2026
                     # print(f"On va ajouter le livre numéro {id} à la base")
                     goncourt_instance.add_book_to_selection(president, id, selection)
 
-                print ("Sélection bien effectuée")
+                print("Sélection bien effectuée")
                 input("Appuyez sur la touche 'Entrée' pour retourner au menu")
             # Item : Président : Après le dernier scrutin, indiquer les votes pour les livres et le lauréat
             case 4:
-                print("****************** Saisie des votes pour les livres de la dernière sélection, et attribution du lauréat ******************")
+                print("******* Saisie des votes pour les livres de la dernière sélection, et attribution du lauréat *******")
                 # On vérifie si la deuxième ET la troisième sélection ont pas déjà été faites
-                if (not is_selection_2_already or not is_selection_3_already):
+                if not is_selection_2_already or not is_selection_3_already:
                     print("ERREUR - Les sélections n'ont pas encore toutes été renseignées, il n'est pas encore possible de réaliser cette action.")
                     input("Appuyez sur la touche 'Entrée' pour retourner au menu")
                     continue
                 # On vérifie que la sélection du lauréat n'a pas déjà été effectuée
-                elif (is_selection_4_already):
+                elif is_selection_4_already:
                     print("ERREUR - Les votes de la dernière sélection et la désignation du lauréat ont déjà été effectués, il est impossible de réaliser cette action.")
                     input("Appuyez sur la touche 'Entrée' pour retourner au menu")
                     continue
@@ -170,10 +166,10 @@ Prix Goncourt 2026
                     print("Voici les livres disponibles : ")
                     list_books_availables: list[Book] = goncourt_instance.get_books_by_selection(num_selection - 1)
 
-                    #On va conserver celui qui a le plus de votes
+                    # On va conserver celui qui a le plus de votes
                     max_votes = 0
-                    id_max_votes = 0
-                    #Pour chaque livre, on va demander au Président le nombre de votes obtenu
+                    # id_max_votes = 0
+                    # Pour chaque livre, on va demander au Président le nombre de votes obtenu
                     for b in list_books_availables:
                         print(f"Livre Numéro {b.id_book} Titre : {b.title} ")
                         nb_votes = get_int_input("Combien de votes ce livre a t'il obtenu ? \n", 0, 10)
