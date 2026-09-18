f75d8d2c05748b47a6048e54e0eb3f32049b9d42
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-09-18 16:06:25 +0200

    corrections warning jury.py

--- models/jury.py
+++ models/jury.py
@@ -9,14 +9,8 @@ class Jury:
     """Membre du jury"""
     last_name: str
     first_name: str
-    #id_jury: int
     id_editor: Optional[int] = 0
-
-    """
-    def __init__(self, last_name: str, first_name: str) -> None:
-        self.last_name = last_name
-        self.first_name = first_name
-        self.id_jury = 0"""
+    id_jury: Optional[int] = 0
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
